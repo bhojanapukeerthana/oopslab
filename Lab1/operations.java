ac6837acfa36e9f7aef01f69579cbe6e64657719
bhojanapukeerthana <bhojanapukeerthana@gmail.com>
2025-12-23 22:04:43 +0530

    Add files via upload

---
+++ Lab1/operations.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class operations{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter the first number:");
+    int a = sc.nextInt();
+    System.out.print("enter the second number:");
+    int b = sc.nextInt();
+    int sum = a + b;
+    int diff = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(mul);
+    System.out.println(div);
+    System.out.print(mod);
+  }
+}
