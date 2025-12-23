c66514413087116aa7c1eb2c2f31511f77aaa829
bhojanapukeerthana <bhojanapukeerthana@gmail.com>
2025-12-23 22:01:52 +0530

    Add files via upload

---
+++ Lab1/conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals{
+  public static void main(String args[]){
+   int a = 66;
+   int b = 44;
+   if (a > b){
+     System.out.println("a is greater than b");
+   }
+   else {
+     System.out.println("a is less than b");
+   }
+  } 
+}
