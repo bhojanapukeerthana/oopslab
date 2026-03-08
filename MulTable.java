08c93ab9377448a7c77c6510c6f773f51f4fea45
bhojanapukeerthana <bhojanapukeerthana@gmail.com>
2026-03-08 21:40:26 +0530

    Add files via upload

---
+++ MulTable.java
@@ -0,0 +1,14 @@
+// multiplication of numbers
+import java.util.Scanner;
+public class MulTable{
+ public static void main(String args []){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("enter the number n");
+  int n = sc.nextInt();
+    for(int i=1;i<=10;i++){
+    System.out.println(n + "*" + i + "=" + (n*i));
+
+  }
+ 
+ }
+}
