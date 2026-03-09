cd55a19c7c82a0918cd81b6227cc0709c5250664
bhojanapukeerthana <bhojanapukeerthana@gmail.com>
2026-03-09 13:26:16 +0530

    Add files via upload

---
+++ conditional statements/Primenum.java
@@ -0,0 +1,11 @@
+public class Primenum{
+ public static void main(String args []){
+  int num = 4;
+  if(num % 10 ==0){
+     System.out.println("the given number is prime number");
+  }
+  else{
+   System.out.println("the given number is not a prime number");
+  }
+ }
+}
---
+++ conditional statements/Weekday.java
@@ -0,0 +1,9 @@
+public class Weekday{
+ public static void main(String args []){
+  String day = "Sunday";
+  switch (day){
+   case "Sunday" -> System.out.println("Its fun day");
+   default -> System.out.println("its not a day");
+  }
+ }
+}
