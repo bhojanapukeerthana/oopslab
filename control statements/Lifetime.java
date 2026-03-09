e771e6381aa7cd91afb6e979c792dd2da943d7fe
bhojanapukeerthana <bhojanapukeerthana@gmail.com>
2026-03-09 13:28:27 +0530

    Add files via upload

---
+++ control statements/Lifetime.java
@@ -0,0 +1,11 @@
+public class Lifetime{
+ public static void main(String args []){
+  int x = 0;
+  for (; x<3;x++){
+  int y =-1;
+  System.out.println("y is:"+y);
+  y = 100;
+  System.out.println("y is now:"+y);
+  }
+ }
+}
